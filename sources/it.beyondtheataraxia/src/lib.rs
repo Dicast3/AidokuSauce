60ddfb57cd15abd8f7f25ace139a9b558bdabb3a
Mattia <mattia.maff03@gmail.com>
2026-07-03 17:10:17 +0200

    Add files via upload

---
+++ sources/it.beyondtheataraxia/src/lib.rs
@@ -0,0 +1,32 @@
+#![no_std]
+use aidoku::{Source, Viewer, prelude::*};
+use madara::{Impl, Madara, Params};
+
+const BASE_URL: &str = "https://www.beyondtheataraxia.com";
+
+struct Beyond;
+
+impl Impl for Beyond {
+    fn new() -> Self {
+        Self
+    }
+
+    fn params(&self) -> Params {
+        Params {
+            base_url: BASE_URL.into(),
+            source_path: "manga-list".into(),
+            use_style_images: true,
+            datetime_format: "dd MMMM yyyy".into(),
+            default_viewer: Viewer::RightToLeft,
+            page_list_selector: "div.reading-content img.wp-manga-chapter-img".into(),
+            ..Default::default()
+        }
+    }
+}
+
+register_source!(
+    Madara<Beyond>,
+    DeepLinkHandler,
+    MigrationHandler,
+    ImageRequestProvider
+);
